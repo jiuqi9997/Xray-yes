86ab7d400ceeabc661500d8a920f805b24efb27e
Signaliks <youngdo@jiangxia.gov.cn>
2021-02-08 21:01:08 +0800

    Fix a variable problem and correct capitalization.

--- xray-yes.sh
+++ xray-yes.sh
@@ -8,7 +8,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 stty erase ^?
-script_version="1.1.10"
+script_version="1.1.11"
 nginx_dir="/etc/nginx"
 nginx_conf_dir="/etc/nginx/conf"
 website_dir="/home/wwwroot"
