029f8bbc05c5ee21fb445cceb4ed7499fb114073
Signaliks <youngdo@jiangxia.gov.cn>
2021-02-08 21:00:33 +0800

    Fix a variable problem and correct capitalization.
    Fix a path of nginx general configuration file.
    Correct the case of the word Linux.

--- xray-yes-en.sh
+++ xray-yes-en.sh
@@ -8,7 +8,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 stty erase ^?
-script_version="1.1.10"
+script_version="1.1.11"
 nginx_dir="/etc/nginx"
 nginx_conf_dir="/etc/nginx/conf"
 website_dir="/home/wwwroot"
@@ -565,7 +565,7 @@ http
 
 		server_tokens off;
 		access_log off;
-		include /etc/nginx/conf/vhost/*.conf;
+		include $nginx_conf_dir/vhost/*.conf;
 }
 EOF
 }
@@ -602,7 +602,7 @@ finish() {
 	echo -e "$Red Host：$Font $xray_domain" | tee -a $info_file
 	echo -e "$Red TLS：$Font ${RedBG}XTLS${Font}" | tee -a $info_file
 	echo ""
-	echo -e "${GreenBG} Tip: ${Font}You can use flow control ${RedBG}xtls-rprx-splice${Font} on the linux platform to get better performance"
+	echo -e "${GreenBG} Tip: ${Font}You can use flow control ${RedBG}xtls-rprx-splice${Font} on the Linux platform to get better performance"
 }
 
 update_xray() {
